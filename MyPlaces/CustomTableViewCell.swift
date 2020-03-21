bc60970a39626bbe5b26d3c69f9e35641ebc77ae
Tarasenko Serhii <blindsuricate@gmail.com>
2020-03-21 17:47:13 +0200

    CustomTableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by user on 2020-03-21.
+//  Copyright © 2020 TarasenkoSerhii. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+
+
+}
